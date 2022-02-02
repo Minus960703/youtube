23e7392ffe282ec290e6e6e8a4ed6e8f5419d07a
Minus960703 <55120608+Minus960703@users.noreply.github.com>
2022-02-02 22:01:06 +0900

    add to youtube api key service
    mvc pattern service -> http network storage, updated

--- src/app.jsx
+++ src/app.jsx
@@ -4,30 +4,17 @@ import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
   const search = query => {
-    var requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-
-    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyD-DDzktAGHHVJ3_VgSNjdWMw7F5LpWK4Y`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({ ...item, id: item.id.videoId })))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+    youtube
+      .search(query)
+      .then(videos => setVideos(videos));
   };
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-
-    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyD-DDzktAGHHVJ3_VgSNjdWMw7F5LpWK4Y", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    youtube
+      .mostPopular()
+      .then(videos => setVideos(videos));
   }, []); //[]는 Mount가 되었을때만, 호출 [name]은 name이 업데이트 될때마다 호출
   return (
     <div className={styles.app}>
