6fa775ba7ecc5e0cab771c9375f79dc2e8af8332
Minus960703 <55120608+Minus960703@users.noreply.github.com>
2022-02-02 21:07:34 +0900

    css and jsx component add
    component and jsx and css add

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import styles from './video_item.module.css';
+const VideoItem = ({ video: { snippet } }) => (
+  <li className={styles.container}>
+    <div className={styles.video}>
+      <img className={styles.thumbnail} src={snippet.thumbnails.medium.url} alt="video thumbnail" />
+      <div className={styles.metadata}>
+        <p className={styles.title}>{snippet.title}</p>
+        <p className={styles.channel}>{snippet.channelTitle}</p>
+      </div>
+    </div>
+  </li>
+);
 
-const VideoItem = props => <h1>{props.video.snippet.title}</h1>;
+// const VideoItem = props => <li> //videoItem으로 사용가능.
+//   <img src={props.video.snippet.medium.url} alt="video thumbnail" />
+//   <div>
+//     <p>{props.video.snippet.title}</p>
+//     <p>{props.video.snippet.channelTitle}</p>
+//   </div>
+// </li>
 
 export default VideoItem;
